4e366f3ed864728b1874c3c7df1a001c1a7726f6
AbdulHeisenberg <81732957+wdrtsptn@users.noreply.github.com>
2026-01-21 15:59:45 +0700

    Update match.js

--- noltrax-match/match.js
+++ noltrax-match/match.js
@@ -1,1 +1,26 @@
+let events = [];
 
+function loadVideo() {
+  const url = document.getElementById("videoUrl").value;
+  const id = url.split("v=")[1] || url.split("/").pop();
+  document.getElementById("player").src =
+    `https://www.youtube.com/embed/${id}?enablejsapi=1&autoplay=1`;
+}
+
+function tagEvent(tag) {
+  const time = new Date().toLocaleTimeString();
+
+  const event = {
+    tag,
+    time,
+    note: ""
+  };
+
+  events.push(event);
+
+  const li = document.createElement("li");
+  li.textContent = `${time} - ${tag}`;
+  document.getElementById("log").appendChild(li);
+
+  console.log(events);
+}
