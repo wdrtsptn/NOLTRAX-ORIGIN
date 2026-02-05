e292aa242ab62ef9d6ef017bf05d59153c41859a
AbdulHeisenberg <81732957+wdrtsptn@users.noreply.github.com>
2026-02-05 09:23:16 +0700

    Update match.js

--- noltrax-match/match.js
+++ noltrax-match/match.js
@@ -221,7 +221,7 @@ const editActionsBtn = document.createElement("button");
 editActionsBtn.innerText = "Edit Actions";
 editActionsBtn.style.cssText = `
   position: absolute;
-  top: 10px;
+  top: 5px;
   right: 10px;
   z-index: 50;
   padding: 10px 16px;
